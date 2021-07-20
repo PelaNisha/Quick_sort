4475cd0c13bd18f31b7214391486e28954684b5d
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2021-07-20 22:51:59 +0545

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int lb, ub;
+
+//swap function
 int swap(int *x, int *y)
 {
 	int temp = *x;
@@ -12,51 +14,53 @@ int swap(int *x, int *y)
 	*y= temp;
 }
 
+//partition function
 int partition(int a[], int lb, int ub)
 {
-	int pivot = a[lb];
-	int start = lb;
+	int pivot = a[lb];//seting the pivot value by the left bound 
+	int start = lb;//intialising left bound as start and right bound as end
 	int end = ub;
-	while(start<end)
+	while(start<end)// condtion
 	{
-		while(a[start]<=pivot&& start<end)
+		while(a[start]<=pivot&& start<end)//if element is less than or equal to pivot and start <end then increment start
 		{
 			start++;
 		}
-		while(a[end]>pivot)
+		while(a[end]>pivot)//if a[end]>pivot then decrement end
 		{
 			end--;
 		}
-		if(start<end)
+		if(start<end)// after the above statements, swap the start and end elements since a[start]>a[end] and greter element are placed in right side
 		{
 			swap(a[start],a[end]);
 		}
 	}
-	swap(a[lb], a[end]);
-	return (end);
+	swap(a[lb], a[end]);// when the end<start then swap the a[lb] and a[end]
+	return (end);//return the end
 }
 
+//quickSort function
 int Quicksort(int a[], int lb,int  ub)
 {
 	int loc;
 	if(lb<ub)
 	{
 	
-		loc = partition(a, lb, ub);
-		Quicksort(a,lb, loc-1);
-		Quicksort(a, loc+1,ub);	
+		loc = partition(a, lb, ub);//calling partition function
+		Quicksort(a,lb, loc-1);//calling the quicksort fucntion recursively for the left sort
+		Quicksort(a, loc+1,ub);	//calling the quicksort fucntion recursively for right sort
 	}
 	
 }
-
+//printArray function
 void printArray(int a[], int s)
 {
     int i;
-    for (i=0; i < s; i++)
+    for (i=0; i < s; i++)//display the array
         printf("%d ", a[i]);
     printf("\n");
 }
-
+//main function
 int main()
 {
 	int size;
@@ -70,7 +74,6 @@ int main()
 	}
 	lb = 0;
 	ub = size-1;
-//	partition(a,lb,ub );
 	Quicksort(a,lb,ub );
 	printArray(a, size);
 	
